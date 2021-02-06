12fdb7eaa2ca513772e1792985af83cf8ad6905a
Shreyash Saitwal <saitwalshreyash19@gmail.com>
2021-02-06 22:11:23 +0530

    Update template files

--- lib/templates/extension_template.dart
+++ lib/templates/extension_template.dart
@@ -2,28 +2,26 @@ String getExtensionTemp(String name, String org) {
   return '''
 package $org;
 
-import com.google.appinventor.components.annotations.DesignerComponent;
-import com.google.appinventor.components.annotations.SimpleObject;
-import com.google.appinventor.components.common.ComponentCategory;
-import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
-import com.google.appinventor.components.runtime.AndroidViewComponent;
-import com.google.appinventor.components.runtime.ComponentContainer;
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.*;
+import com.google.appinventor.components.runtime.*;
 
-@SimpleObject(external = true)
+import java.util.List;
+
 public class $name extends AndroidNonvisibleComponent {
 
   public $name(ComponentContainer container) {
     super(container.\$form());
   }
 
-  // @SimpleFunction(description = "A method that returns the sum of all digits in a list.")
-  // public int GetSumFromList(YailList list) {
-  //   int sum = 0;
-  //   for (int i : list) {
-  //     sum += i;
-  //   }
-  //   return sum;
-  // }
+  @SimpleFunction(description = "Returns the sum of all numbers in a list.")
+  public int SumAll(List<Integer> list) {
+    int sum = 0;
+    for (int i : list) {
+      sum += i;
+    }
+    return sum;
+  }
 
 }''';
 }
--- lib/templates/rush_yaml_template.dart
+++ lib/templates/rush_yaml_template.dart
@@ -4,19 +4,21 @@ name: $name
 description: Extension component for $name. Created using Rush.
 
 version:
-  number: auto
+  number: auto          # Auto increaments version number when built with '-r' (or '--release') flag.
   name: $versionName
 
-assets:
-  icon: icon.png        # Extension icon
+# Doesn't work
+# assets:
+  # icon: icon.png      # Extension icon
   # other:              # Extension asset(s)
   #   - my_awesome_pic.png
 
 authors:
   - $author
 
+# Untested
 # dependencies:
-#   - my_awesome_library.[jar/aar]
+#   - my_awesome_library.jar
 
 ''';
 }
