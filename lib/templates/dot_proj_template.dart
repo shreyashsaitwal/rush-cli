21ad9ef9dc55ddb10825eda79bca9506b698ce26
Shreyash <saitwalshreyash19@gmail.com>
2021-02-18 23:26:46 +0530

    Simplify .project template

--- lib/templates/dot_proj_template.dart
+++ lib/templates/dot_proj_template.dart
@@ -1,5 +1,4 @@
 String getDotProject(String name) {
-  final time = DateTime.now().toString().replaceAll('-', '').replaceAll(':', '').replaceAll('.', '').replaceAll(' ', '');
   return '''
 <?xml version="1.0" encoding="UTF-8"?>
 <projectDescription>
@@ -17,17 +16,6 @@ String getDotProject(String name) {
 	<natures>
 		<nature>org.eclipse.jdt.core.javanature</nature>
 	</natures>
-	<filteredResources>
-		<filter>
-			<id>${time.substring(time.length - 13)}</id>
-			<name></name>
-			<type>30</type>
-			<matcher>
-				<id>org.eclipse.core.resources.regexFilterMatcher</id>
-				<arguments>node_modules|.git|__CREATED_BY_JAVA_LANGUAGE_SERVER__</arguments>
-			</matcher>
-		</filter>
-	</filteredResources>
 </projectDescription>
 
 ''';
