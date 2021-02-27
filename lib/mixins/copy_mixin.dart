a0dee4294b320afc0074e034d505bd263373d4f6
Shreyash <saitwalshreyash19@gmail.com>
2021-02-27 22:26:11 +0530

    Move build script into scripts directory

--- lib/mixins/copy_mixin.dart
+++ lib/mixins/copy_mixin.dart
@@ -2,13 +2,12 @@ import 'package:path/path.dart' as p;
 import 'dart:io' show Directory, File;
 
 mixin CopyMixin {
-  /// Copies the contents of [source] dir to the [dest] dir leaving the optional
-  /// files/dirs listed in the [leave].
+  /// Copies the contents of [source] dir to the [dest] dir.
   void copyDir(Directory source, Directory dest) {
     var files = source.listSync();
     files.forEach((entity) async {
       if (entity is File) {
-        await entity.copySync(p.join(dest.path, p.basename(entity.path)));
+        entity.copySync(p.join(dest.path, p.basename(entity.path)));
       } else if (entity is Directory) {
         var newDest = Directory(p.join(dest.path, entity.path.split('\\').last));
         newDest.createSync();
--- lib/templates/rush_yaml_template.dart
+++ lib/templates/rush_yaml_template.dart
@@ -4,7 +4,7 @@ name: $name           # Caution: DO NOT change the name.
 description: Extension component for $name. Created using Rush.
 
 version:
-  number: auto        # Auto increaments version number when built with '-r' (or '--release') flag.
+  number: auto        # Auto increments version number when built with '-r' (or '--release') flag.
   name: $versionName
 
 assets:
@@ -15,7 +15,7 @@ assets:
 authors:
   - $author
 
-# deps:         # Depenedencies should be first added to the "deps" folder
+# deps:         # Dependencies should be first added to the "deps" folder
 #   - my_awesome_library.jar
 
 ''';
