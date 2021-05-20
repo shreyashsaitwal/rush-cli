fcb2ab76488ae39536ca58414e82aff16de34d4b
Shreyash <saitwalshreyash19@gmail.com>
2021-05-20 20:49:18 +0530

    Release: 1.1.2 (hotfix)

--- lib/version.dart
+++ lib/version.dart
@@ -1,3 +1,3 @@
 // Auto-generated. Do not modify.
-const String rushVersion = '1.1.1';
-const String rushBuiltOn = '2021-05-19 08:23:11.238013Z';
+const String rushVersion = '1.1.2';
+const String rushBuiltOn = '2021-05-20 15:24:07.920824Z';
