9a6579921f29852a353351ac457af2d272ebe595
Shreyash <saitwalshreyash19@gmail.com>
2021-07-29 18:39:43 +0530

    chore: don't enable desugaring by default in new projects

--- lib/templates/rush_yml.dart
+++ lib/templates/rush_yml.dart
@@ -1,39 +1,38 @@
 String getRushYamlTemp(
     String name, String versionName, String author, bool enableKt) {
   return '''
-# For a detailed info on this file and supported fields, check
-# out this link: https://github.com/ShreyashSaitwal/rush-cli/wiki/Metadata-File
+# For a detailed info on this file and supported fields, check out the below link:
+# https://github.com/ShreyashSaitwal/rush-cli/wiki/Metadata-File
 
 ---
-name: $name           # Caution: DO NOT change the name.
+name: $name
 description: Extension component for $name. Created using Rush.
 
 version:
-  number: auto        # Auto increments version number when built with '-r' ('--release') flag.
+  number: auto
   name: '$versionName'
 
 authors:
   - $author
 
 build:
+  release:
+    # Optimizes the extension on every release build.
+    optimize: true
+${enableKt ? _getKtField() : ''}
   # If enabled, you will be able to use Java 8 language features in your extension.
   desugar:
-    enable: true
+    enable: false
     desugar_deps: false
-${enableKt ? _getKtField() : ''}
-  # Optimizes your extension on every release build.
-  release:
-    optimize: true
 
 assets:
-  # Extension icon
+  # Extension icon. This can be a URL or a local image in 'assets' folder.
   icon: icon.png
-
-  # Extension asset(s)
+  # Extension assets.
   # other:
   #   - my_awesome_asset.anything
 
-# Dependencies declared here should be present in the "deps" directory.
+# Extension dependencies (JAR).
 # deps:
 #   - my_awesome_library.jar
 ''';
