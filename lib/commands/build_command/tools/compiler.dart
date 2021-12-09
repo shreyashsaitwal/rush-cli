cd6faedd8c0988382bc51bba41233113bb2d3aff
Shreyash <saitwalshreyash19@gmail.com>
2021-12-09 16:47:58 +0530

    fix: specify character encoding when invoking javac

--- lib/commands/build_command/tools/compiler.dart
+++ lib/commands/build_command/tools/compiler.dart
@@ -132,6 +132,7 @@ class Compiler {
     final args = <String>[];
 
     args
+      ..addAll(['-encoding', 'UTF8'])
       ..addAll(['-d', classesDir.path])
       ..addAll(['-cp', classpath])
       ..addAll([...apArgs])
