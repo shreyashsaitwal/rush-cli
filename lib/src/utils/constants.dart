a2bb725609a9e3e561a8f647c073f5425d48db15
Shreyash Saitwal <saitwalshreyash19@gmail.com>
2022-11-13 16:53:25 +0530

    chore: update processor & ai2rt version

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -8,7 +8,7 @@ const androidManifestTimestampKey = 'android-manifest-xml';
 
 const defaultKtVersion = '1.7.10';
 
-const annotationProcVersion = '2.0.0';
-const ai2RuntimeVersion = 'nb190b';
+const annotationProcVersion = '2.0.1';
+const ai2RuntimeVersion = 'nb190b.1';
 
 const androidPlatformSdkVersion = '33';
